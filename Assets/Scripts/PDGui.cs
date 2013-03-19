c90ff6b6fe6651e94550264b352cc0281512b61f
Bronson Zgeb <bronson.zgeb@gmail.com>
2013-03-19 19:41:24 -0400

    Added several new buttons for testing

--- Assets/Scripts/PDGui.cs
+++ Assets/Scripts/PDGui.cs
@@ -3,14 +3,35 @@ using System.Collections;
 
 public class PDGui : MonoBehaviour {
     void OnGUI() {
-        GUILayout.BeginArea( new Rect(20, 20, 100, 1000) );
+        GUILayout.BeginArea( new Rect(20, 20, Screen.width - 40, 1000) );
         GUILayout.BeginVertical();
-        if ( GUILayout.Button( "Open File" ) ) {
+        if ( GUILayout.Button( "Open File", GUILayout.Height( 50 ) ) ) {
             PureData.openFile("basicsynth.pd");
         }
+
         GUILayout.Space(15);
-        if ( GUILayout.Button( "Close File" ) ) {
+        if ( GUILayout.Button( "Close File", GUILayout.Height( 50 ) ) ) {
             PureData.closeFile("basicsynth.pd");
+        }
+
+        GUILayout.Space(15);
+        if ( GUILayout.Button( "Start", GUILayout.Height( 50 ) ) ) {
+            PureData.startAudio();
+        }
+
+        GUILayout.Space(15);
+        if ( GUILayout.Button( "Pause", GUILayout.Height( 50 ) ) ) {
+            PureData.pauseAudio();
+        }
+
+        GUILayout.Space(15);
+        if ( GUILayout.Button( "Stop", GUILayout.Height( 50 ) ) ) {
+            PureData.stopAudio();
+        }
+
+        GUILayout.Space(15);
+        if ( GUILayout.Button( "Send Float", GUILayout.Height( 50 ) ) ) {
+            PureData.sendFloat(Random.Range(40, 127), "note");
         }
         GUILayout.EndVertical();
         GUILayout.EndArea();
